88e82531aea2d1f5669d59fcaae5a38e298e5d72
Shimolee Jhaveri <shimoleejhaveri@gmail.com>
2020-04-08 22:16:32 +0000

    Solutions w/ questions

--- solution8.py
+++ solution8.py
@@ -4,8 +4,6 @@ of linked list.
 
 If there are two middle nodes, return the second middle node.
 
- 
-
 Example 1:
 Input: [1,2,3,4,5]
 Output: Node 3 from this list (Serialization: [3,4,5])
@@ -17,7 +15,6 @@ Example 2:
 Input: [1,2,3,4,5,6]
 Output: Node 4 from this list (Serialization: [4,5,6])
 Since the list has two middle nodes with values 3 and 4, we return the second one.
- 
 
 Note:
 The number of nodes in the given list will be between 1 and 100.

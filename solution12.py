509bbcf841de19c84a025535dc7569ecaf21b770
Shimolee Jhaveri <shimoleejhaveri@gmail.com>
2020-04-17 21:03:51 +0000

    Solutions w/ questions

--- solution12.py
+++ solution12.py
@@ -1,11 +1,13 @@
 """
 We have a collection of stones, each stone has a positive integer weight.
 
-Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+Each turn, we choose the two heaviest stones and smash them together.  Suppose 
+the stones have weights x and y with x <= y.  The result of this smash is:
 
 If x == y, both stones are totally destroyed;
-If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
-At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+If x != y, the stone of weight x is totally destroyed, and the stone of weight y
+has new weight y-x. At the end, there is at most 1 stone left.  Return the weight 
+of this stone (or 0 if there are no stones left.)
 
 Example 1:
 Input: [2,7,4,1,8,1]
@@ -14,8 +16,8 @@ Explanation:
 We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
 we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
 we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
-we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
- 
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of 
+last stone.
 
 Note:
 1 <= stones.length <= 30
@@ -43,10 +45,10 @@ class Solution:
                     x = stones.pop()
                     z = (y - x)
                     stones.append(z)
-                    
+
                 else:
                     y = stones.pop()
                     x = stones.pop()
                     z = (y - x)
                     stones.append(z)
-
+                    
